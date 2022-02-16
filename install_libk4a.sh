372979cabd4c30200d31cbc42f1feca66410f48f
Rui Luo <luo.rui@husky.neu.edu>
2022-02-16 11:54:45 -0500

    Update install_libk4a.sh

--- install_libk4a.sh
+++ install_libk4a.sh
@@ -8,10 +8,10 @@ if [[ $os = "focal" || $os = "bionic" ]]; then
 	  echo "Retrieving azure kinect packages. Be patient..."
 	  curl -sSL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4/libk4a1.4_1.4.1_amd64.deb > /tmp/libk4a1.4_1.4.1_amd64.deb
 	  curl -sSL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/libk/libk4a1.4-dev/libk4a1.4-dev_1.4.1_amd64.deb > /tmp/libk4a1.4-dev_1.4.1_amd64.deb
-      curl -sSL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/k/k4a-tools/k4a-tools_1.4.1_amd64.deb > /tmp/k4a-tools_1.4.1_amd64.deb
+      	  curl -sSL https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/k/k4a-tools/k4a-tools_1.4.1_amd64.deb > /tmp/k4a-tools_1.4.1_amd64.deb
 	#   echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections
 	#   echo 'libk4a1.4 libk4a1.4/accept-eula select true' | sudo debconf-set-selections
-      echo 'libk4a packages downloaded under /tmp folder'
+          echo 'libk4a packages downloaded under /tmp folder'
 	  sudo dpkg -i /tmp/libk4a1.4_1.4.1_amd64.deb
 	  sudo dpkg -i /tmp/libk4a1.4-dev_1.4.1_amd64.deb
 	  sudo dpkg -i /tmp/k4a-tools_1.4.1_amd64.deb
